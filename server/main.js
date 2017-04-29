96b013712588420487a2e7b9f52bc945dcef0d05
Tim Moran <tim@t-moran.com>
2017-04-29 23:17:20 +0100

    Store queued track in pg

--- server/main.js
+++ server/main.js
@@ -1,6 +1,10 @@
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require('express')
+const bodyParser = require('body-parser')
 const morgan = require('morgan')
+const pg = require('pg')
+const connectionString = process.env.DATABASE_URL || 'postgres://localhost:5432/jukebot'
+
+const client = new pg.Client(connectionString)
 
 const PORT = process.env.PORT || 3000;
 
@@ -12,7 +16,21 @@ server.use(morgan('tiny'))
 server.post('/api/enqueue', (req, res) => {
   const track = req.body
   console.log(`Request to enqueue track: ${JSON.stringify(track)}`)
-  res.status(201).json({'status': 'ok'})
+
+  pg.connect(connectionString, (err, client, done) => {
+    console.log('Connected to database')
+    if(err) {
+      done();
+      console.log(err);
+      return res.status(500).json({success: false, data: err});
+    }
+
+    const q = client.query('insert into queued_tracks (spotify_uri) values ($1)', [track.spotifyUri])
+    q.on('end', () => {
+      done()
+      return res.status(201).json({'status': 'ok'})
+    })
+  })
 });
 
 server.listen(PORT, () => {
--- server/sonos.js
+++ server/sonos.js
@@ -5,12 +5,59 @@ const discovery = new SonosSystem({});
 var currentIndex = 0;
 var addingToQueue = false;
 
+// extract tracks from playlist json:
+// cat playlist.json | jq '[.tracks.items[].track.uri]'
 const trackUris = [
-  'spotify:track:6fTsxhIosAU6zXTAhSaemI',
-  'spotify:track:1n7D2aCuzDoFTXSje54YA8',
-  'spotify:track:4j0Vn3lI1SHtI4bvJSM47e',
-  'spotify:track:7sFPfxWORT9q7WcvkUyZeL',
-  'spotify:track:18zyVulK9H3peQTaLlqbGe'
+  "spotify:track:6gOirCUz62z0mFhCJ6P5KC",
+  "spotify:track:1Rvl8qsKJurfFTyWLBI9ib",
+  "spotify:track:5ByIHT8s38diBQf6dkEWbt",
+  "spotify:track:40dJCw4xU6Bd5ie9rfagNo",
+  "spotify:track:2XCI7vQB1pnGR3nuPtoejf",
+  "spotify:track:0ky5BaGgJJnYATgz2KAc1q",
+  "spotify:track:43VSZHjLjjxSi7KosnHZ8w",
+  "spotify:track:04tphyAEH1SJFp6beHQdaz",
+  "spotify:track:43L7sXzJLKnto5PpKLf28r",
+  "spotify:track:18n3RlT0UNcy2yHImHy8IT",
+  "spotify:track:44L7aDHaRzb9pMWEZjSH8u",
+  "spotify:track:222iv52N3sRK2iTJGlfGq0",
+  "spotify:track:6hdNOhGNR1x0q7xPfOfYhm",
+  "spotify:track:3TjlMH27nWbY3veJ8fHdaD",
+  "spotify:track:6cpk00i5TxCqSeqNi2HuIe",
+  "spotify:track:3WkPRXJvKa7tRAvx5g0idb",
+  "spotify:track:3RNyGLgSvmVRZ7xKUp8Wgd",
+  "spotify:track:55A8N3HXzIecctUSvru3Ch",
+  "spotify:track:64LEzmN3831SPhxGIBQ4KH",
+  "spotify:track:52Rp3xBJFYYdmpgzDy0Quf",
+  "spotify:track:2hbA72TFulrNGTEBUMAnzG",
+  "spotify:track:1Ho2mFQ415Q97rEe5hSO1x",
+  "spotify:track:1MkcvwNhKiE5vyrw73xZtY",
+  "spotify:track:5SI6jYhKUWm1qnaheTm4Zw",
+  "spotify:track:7kTvXtMERxvwCGcWv68ssH",
+  "spotify:track:0vFs2Eorve6vnnQcmItot1",
+  "spotify:track:34zWZOSpU2V1ab0PiZCcv4",
+  "spotify:track:7INi4pMPG4IE0Smx5y4KVf",
+  "spotify:track:7Bz8yww6UMbTgTVLG6zbI4",
+  "spotify:track:4Xgr8IWwGSGyqLjwiwOxqk",
+  "spotify:track:6B1mi7lMKxdSuD40kxAIs9",
+  "spotify:track:1F6A8AHmzEyzPSrEc9aumd",
+  "spotify:track:6Fj36toK6ky8HhyZdVTsce",
+  "spotify:track:6XitzIBDSAbU9gU9lhouQa",
+  "spotify:track:7cKLOenzViwzoHJWciMIrj",
+  "spotify:track:680kMGV6szhTiZbHRr3vLi",
+  "spotify:track:2lPfcRMRLffiBTwLA9S8Vc",
+  "spotify:track:4MM4UlT0j7MePs9bMoi34N",
+  "spotify:track:1Sp1rSN58hcQhdMzqk4IcE",
+  "spotify:track:3ldG6XCLDXxWg1N6XhrSKo",
+  "spotify:track:386RUes7n1uM1yfzgeUuwp",
+  "spotify:track:1I6q6nwNjNgik1Qe8Oi0Y7",
+  "spotify:track:3sFEGvMPkY8Ti97K94Pwp9",
+  "spotify:track:0INtJm8gCJKtNaAYQNVlpZ",
+  "spotify:track:6HHrXbBPk5ybtKuYG9SZDH",
+  "spotify:track:6OJq6oyCcsJUg4fENeNJk4",
+  "spotify:track:3yGdLMObhf3IbmzfLohAAh",
+  "spotify:track:2SE11vmjbi7j5QKJ885ib6",
+  "spotify:track:7LOAChK8Lkj8ZdF1LuhNWP",
+  "spotify:track:4OsZ1vrenrtSbqLJxOceKl"
 ];
 
 // var country = '';
@@ -49,11 +96,42 @@ const trackUris = [
 //   }
 // }
 
+const pg = require('pg')
+const connectionString = process.env.DATABASE_URL || 'postgres://localhost:5432/jukebot'
+const client = new pg.Client(connectionString)
+
 function fetchAndQueueNextTrack (player) {
   // getAccountId(player, 'Spotify')
   addingToQueue = true
-  const spotifyUri = trackUris[currentIndex]
-  currentIndex = (currentIndex + 1) % trackUris.length
+  pg.connect(connectionString, (err, client, done) => {
+    const query = client.query('select id, request_time, spotify_uri from queued_tracks where queued = false order by request_time asc limit 1')
+    var result = undefined;
+    query.on('row', function (row) {
+      result = row;
+    })
+    query.on('end', function () {
+      if (result !== undefined) {
+        const spotifyUri = result.spotify_uri
+        const delQueue = client.query('update queued_tracks set queued = true where id = $1', [result.id])
+        queueTrack(player, spotifyUri).then(() => {
+          console.log('Next track queued successfully!')
+          addingToQueue = false
+        })
+      }
+      else {
+        const spotifyUri = trackUris[currentIndex]
+        currentIndex = (currentIndex + 1) % trackUris.length
+        queueTrack(player, spotifyUri).then(() => {
+          console.log('Next track queued successfully!')
+          addingToQueue = false
+        })
+      }
+    })
+  })
+
+}
+
+function queueTrack(player, spotifyUri) {
   console.log(`Next track: ${spotifyUri}`)
   const encodedSpotifyUri = encodeURIComponent(spotifyUri);
   const serviceType = player.system.getServiceType('Spotify')
@@ -70,11 +148,9 @@ function nextTrackIsEmpty (state) {
   return state.nextTrack.uri.length === 0 && !addingToQueue;
 }
 function getSpotifyMetadata(uri, serviceType) {
-  const title = `Spotify track ${currentIndex}`
   const encodedId = encodeURIComponent(decodeURIComponent(uri).substring('spotify:track:'.length))
   return `<DIDL-Lite xmlns:dc="http://purl.org/dc/elements/1.1/" xmlns:upnp="urn:schemas-upnp-org:metadata-1-0/upnp/" xmlns:r="urn:schemas-rinconnetworks-com:metadata-1-0/" xmlns="urn:schemas-upnp-org:metadata-1-0/DIDL-Lite/">
         <item id="00032020${uri}" restricted="true">
-            <dc:title>${title}</dc:title>
             <upnp:class>object.item.audioItem.musicTrack</upnp:class>
             <desc id="cdudn" nameSpace="urn:schemas-rinconnetworks-com:metadata-1-0/">SA_RINCON${serviceType}_X_#Svc${serviceType}-0-Token</desc>
         </item>
@@ -88,15 +164,12 @@ discovery.on('transport-state', (player) => {
 
     if (nextTrackIsEmpty(player.state)) {
       console.log('No next track - fetching and queuing next...')
-      fetchAndQueueNextTrack(player).then(() => {
-        console.log('Next track queued successfully!')
-        addingToQueue = false
-      })
-      .catch((err) => {
-        console.log('Something went wrong :(')
-        console.log(JSON.stringify(err, null, 4))
-        addingToQueue = false
-      })
+      fetchAndQueueNextTrack(player)
+      // .catch((err) => {
+      //   console.log('Something went wrong :(')
+      //   console.log(JSON.stringify(err, null, 4))
+      //   addingToQueue = false
+      // })
     }
   }
 })
